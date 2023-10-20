786a19e842327cdab1d8bf7e5ad409d628452ee7
Asim Hasnain <124768339+asim-quencil@users.noreply.github.com>
2023-10-21 01:38:31 +0530

    first commit9

--- server/db/conn.js
+++ server/db/conn.js
@@ -5,7 +5,10 @@ const mongoose = require("mongoose");
 // const DB =
 // "mongodb+srv://ritik:1234@cluster0.qvqj0ma.mongodb.net/mernstack?retryWrites=true&w=majority";
 
-const DB = "mongodb://localhost:27017";
+const DB =
+	"mongodb+srv://asim-crud:12345@stack-overflow-clone.3mscsti.mongodb.net/?retryWrites=true&w=majority";
+
+// const DB = "mongodb://localhost:27017";
 
 mongoose
 	.connect(DB, {
